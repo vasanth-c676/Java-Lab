5b0e12e13769f21b0ea8a308ad047cddc1228cd2
vasanth-c676 <25cs291@kpriet.ac.in>
2026-09-23 12:36:43 +0530

    Create Employee.java

---
+++ Experiment7/employee/Employee.java
@@ -0,0 +1,20 @@
+package employee;
+
+public class Employee {
+
+    int id;
+    String name;
+    double salary;
+
+    public Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public void displayEmployee() {
+        System.out.println("Employee ID: " + id);
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee Salary: " + salary);
+    }
+}
